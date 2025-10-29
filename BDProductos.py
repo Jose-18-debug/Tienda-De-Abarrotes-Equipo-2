3007909c06f3bae73acd4a0c4ed92c24eca40a80
José María <sa546357@uaeh.edu.mx>
2025-10-29 00:19:55 -0600

    Este es el código de conexión de la base de datos local al Pycharm

---
+++ BDProductos.py
@@ -0,0 +1,20 @@
+import pymysql
+
+try:
+    conexion = pymysql.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        database = "tiendadeabarrotes"
+    )
+    print("Conexión exitosa :)")
+
+    consulta = conexion.cursor()
+    textoConsulta = "SELECT * FROM productosdelatienda"
+    consulta.execute(textoConsulta)
+    resultado = consulta.fetchall()
+    for x in resultado:
+        print(x)
+
+except Exception as err:
+    print("Hubo un error en la conexion",err)
